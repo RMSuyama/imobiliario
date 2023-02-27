b9817fecdb6bc88ada1a24aa2f41026b76c47b15
RMSuyama <rafaelmoreirasuyama@gmail.com>
2023-02-27 20:12:06 -0300

    qwe

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ defineProps({
   <div class="greetings">
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Pokédex</a>
+          <a class="navbar-brand" href="#">Pokaédex</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
