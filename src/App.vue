6fd8606824486f5683cc805148d5b8fd994093ce
RMSuyama <rafaelmoreirasuyama@gmail.com>
2023-03-02 14:08:03 -0300

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 
 <template>
-<nav class="navbar navbar-expand-lg .bg-body-secondary fixed-top" id="navbar">
+<div><nav class="navbar navbar-expand-lg fixed-top" id="navbar">
   <div class="container-fluid">
-    <a class="navbar-brand" href=""><img id="logo" src="./assets/hc.png" alt="Example Image" /></a>
+    <a class="navbar-brand" href=""><img id="logo" src="./assets/teste.html.png" alt="Example Image" /></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -12,22 +12,22 @@
           <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="https://www.instagram.com/suyama.adv/">Fotos</a>
+          <a class="nav-link" href="https://www.instagram.com/suyama.adv/">História</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="https://www.linkedin.com/in/rafael-moreira-suyama-0043bb189/">Spotify</a>
+          <a class="nav-link" href="https://www.linkedin.com/in/rafael-moreira-suyama-0043bb189/">Serviços</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="https://wa.me/5513996267989">Shows</a>
+          <a class="nav-link" href="https://wa.me/5513996267989">Artigos</a>
         </li>
       </ul>
       <div class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Entre em contato comigo!
+            Entre em contato!
           </a>
           
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="https://www.instagram.com/riqcarneiro/"><img id="ico2" src="./assets/instagram.svg" alt="Insta" /> Instagram</a></li>
+            <li><a class="dropdown-item" href="https://www.instagram.com/suyama.adv/"><img id="ico2" src="./assets/instagram.svg" alt="Insta" /> Instagram</a></li>
             <li><a class="dropdown-item" href="https://www.linkedin.com/in/rafael-moreira-suyama-0043bb189/"><img id="ico2" src="./assets/linkedin.svg" alt="Linkedin" /> LinkedIn</a></li>
             <li><a class="dropdown-item" href="https://wa.me/5513996267989"><img id="ico2" src="./assets/whatsapp.svg" alt="Whats" />Whatsapp</a></li>
 
@@ -37,45 +37,142 @@
     </div>
   </div>
 </nav>
-    <div id="sobremim">
 
-      <div class="card-body" id="left">
-        <p> Conheça Henrique Carneiro, um cantor e violonista talentoso que faz a alma do choro e do samba vibrar. Formado no Conservatório de Música, Henrique é um músico experiente e possui um vasto repertório que varia de clássicos a novas composições.</p>
+  <div class="container text-center" id="body">
+    <div class="row">
+      <div class="col-12">
 
-        <p>Com sua habilidade no violão, ele cria melodias que enchem a plateia de emoção e vida, envolvendo a todos em uma atmosfera única e envolvente. Seu estilo único de interpretar o choro e o samba, com a essência da música brasileira, cativa a todos que o ouvem.</p>
+          <div id="sobremim">
 
-        <p>Não perca a oportunidade de ver Henrique Carneiro ao vivo em seu próximo show. Acompanhado de outros músicos talentosos, Henrique promete uma noite inesquecível com muito ritmo, harmonia e emoção. Venha sentir a música brasileira de uma forma única e autêntica, com Henrique Carneiro. Garanta já seu ingresso e venha celebrar conosco!</p>       </div>
+          <div class="card-body" id="left">
+
+          <h1>ADVOGADO IMOBILIÁRIO</h1>
+
+          <h2>Atendimento Personalizado, com Credibilidade, Agilidade e Transparência</h2>
+
+          <p> Se você está precisando de um advogado imobiliário, você veio ao lugar certo! Com uma formação jurídica sólida e uma vasta experiência na área de regularização imobiliária, eu posso oferecer a você uma gama completa de serviços jurídicos de alta qualidade.</p>
+
+          <p>Como ex-corretor de imóveis e técnico em transações imobiliárias, eu compreendo as necessidades e objetivos dos meus clientes e estou comprometido em oferecer soluções personalizadas para cada caso. Com minha expertise em questões como a obtenção de documentos e registros necessários para regularizar imóveis, resolução de problemas de titulação de imóveis, verificação de registros de propriedade e assessoria jurídica em processos de usucapião, você pode ter certeza de que seus problemas serão resolvidos de forma eficiente e profissional.</p>
+
+          <p>Além disso, eu também ofereço serviços de consultoria em transações imobiliárias, como compra, venda e locação de imóveis. Eu trabalho de perto com meus clientes para entender suas necessidades e objetivos e garantir que seus interesses sejam protegidos em todas as negociações imobiliárias.</p>  
+
+          <p>Meu compromisso com a excelência e a atenção aos detalhes garante que você receberá os melhores resultados possíveis. Então, se você quer contar com um advogado experiente e dedicado para ajudá-lo a resolver questões imobiliárias, entre em contato comigo hoje mesmo! Estou ansioso para ajudá-lo a alcançar seus objetivos imobiliários e fornecer a você a tranquilidade que você merece.</p>      
+        
+        </div>
+        </div>
+        </div>
+
+       <div class="col" id="carouselDiv"  >
+              <div id="carousel" class="carousel slide">
+          <div class="carousel-inner">
+           
+            <div class="carousel-item active">
+              <a href="https://www.instagram.com/suyama.adv/"><img src="./assets/como-funciona-a-regularizacao-imobiliaria.jpg"  class="d-block w-100" alt="1.."></a>
+            </div>
+            <div class="carousel-item">
+              <img src="./assets/regularizacao-de-imoveis.jpeg" class="d-block w-100" alt="2..">
+            </div>
+            <div class="carousel-item">
+              <img src="./assets/download.jfif" class="d-block w-100" alt="3..">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
     </div>
+    
+    <div class="row">
+      <div class="col-12">
+        <footer class="navbar navbar-expand-lg .bg-body-secondary" id="foot">
 
-    <div id="carousel" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./assets/violão1.png" class="d-block w-100" alt="1..">
-    </div>
-    <div class="carousel-item">
-      <img src="./assets/violão2.png" class="d-block w-100" alt="2..">
-    </div>
-    <div class="carousel-item">
-      <img src="./assets/violão3.png" class="d-block w-100" alt="3..">
+  <div id="recursos">
+
+                  <h3>Endereço:</h3>
+                   <p><a href="https://www.google.com/maps/@-24.4945346,-47.8467756,21z">Rua Celso Xavier, 590 – Centro – Registro/SP</a></p> 
+                   <p><a href="http://www.google.com">E-mail: rafaelmoreirasuyama@adv.oabsp.org.br</a></p>
+                   <p><a href="http://www.google.com">Telefone de contato</a></p>
+                  </div>
+                  <div id="recursos">
+
+                  <h3>Sobre a Empresa:</h3>
+
+                  <p><a href="http://www.google.com">Nossa história</a></p> 
+                  <p><a href="http://www.google.com">Nossa equipe</a></p>
+                  <p><a href="http://www.google.com">Depoimento de Clientes</a></p>
+
+                  </div>
+
+                  <div id="recursos">
+                  <h3>Recursos:</h3>
+
+                  <p><a href="http://www.google.com">Blog</a></p> 
+                  <p><a href="http://www.google.com">FAQs</a></p>
+                  <p><a href="http://www.google.com">Política de privacidade</a></p>
+                  <p><a href="http://www.google.com">Termos de uso</a></p>
+
+                  </div>
+
+                  <div id="recursos">
+                  <h3>Siga-nos nas redes sociais:</h3>
+                  
+                  <p><a class="dropdown-item" href="https://www.instagram.com/riqcarneiro/"><img id="ico2" src="./assets/instagram.svg" alt="Insta" /> Instagram</a></p>
+                  <p><a class="dropdown-item" href="https://www.linkedin.com/in/rafael-moreira-suyama-0043bb189/"><img id="ico2" src="./assets/linkedin.svg" alt="Linkedin" /> LinkedIn</a></p>
+                  <p><a class="dropdown-item" href="https://wa.me/5513996267989"><img id="ico2" src="./assets/whatsapp.svg" alt="Whats" />Whatsapp</a></p>
+
+                  </div>
+  </footer>
+      </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
 
-<footer class="navbar navbar-expand-lg .bg-body-secondary fixed-bot" id="navbar">teste</footer>
 </template>
 
 <style scoped>
 
+
+h1 {
+  font-size:xx-large;
+  text-align: center;
+  }
+
+h2 {
+  padding-top: 2vw;
+  padding-bottom: 3vw;
+  font-size:large;
+  text-align: center;
+}
+
+h3 {
+  text-align: center;
+}
+
+h3 {
+
+  text-align: center;
+
+}
+
+#recursos{
+  width:25vw;
+  height: 25vh;
+  margin-bottom: 5vw;}
+
 #navbar {
   z-index: 9999;
+}
+
+#foot {
+  display: flex;
+  padding-top: 7vw;
+
 }
 
 #sobremim {
@@ -84,9 +181,27 @@
 }
 
 #logo{
-  width:5vw;
-  height:4.5vw;
+  padding-left: 1.3vw;
+  padding-bottom: 5px;
+  width:7vw;
+  height:5.5vw;
+  display:flex;
+  align-items: center;
+  justify-content: center;
 }
+
+#logo2{
+ 
+  padding-left: 1.3vw;
+  padding-bottom: 5px;
+  width:7vw;
+  height:5.5vw;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
 
 #left{
   text-indent: 20px;
@@ -96,12 +211,30 @@
 }
 
 #ico2{
-  width:1.5vw;
-  height:1.5vw;
+  width:2vw;
+  height:2vw;
   margin-right: 1vw;
 }
 
 #carousel{
-  padding-top:15vh;
+  width:500px;
+  height:300px;
+  align-items: center;
+  justify-items: center;
+  justify-content: center;
+  display: flex;
+
+  }
+  #carouselDiv{
+
+  align-items: center;
+  justify-items: center;
+  justify-content: center;
+  display: flex;
+  } 
+
+#navbar{
+  background-color:white;
 }
+
 </style>
